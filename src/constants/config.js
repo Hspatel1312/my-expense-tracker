d3e9a02da677a345936bc1c7d2b6590364ea8d36
Hspatel1312 <hspatel1312@gmail.com>
2025-07-05 23:41:52 +0530

    Create config.js

---
+++ src/constants/config.js
@@ -0,0 +1,7 @@
+export const SHEETS_CONFIG = {
+  spreadsheetId: '1F_dHrcPRz4KFISVQFnOPYD37VWZBKlkIgyLLm66Enlg',
+  apiKey: 'AIzaSyCxpGczSKU6rtG36mPAlU7I8QuG3Z-gMeo',
+  clientId: '130621204284-j2gk44qb30mvkd4pm7soav68nphtfkok.apps.googleusercontent.com',
+  TRANSACTIONS_RANGE: 'Transactions!A:I',
+  ACCOUNTS_RANGE: 'Data!E:G'
+};
