eabc1754e9f5de89c6d28e25a1687637f8fa9d90
Hspatel1312 <hspatel1312@gmail.com>
2025-07-05 23:41:28 +0530

    Create categories.js

---
+++ src/constants/categories.js
@@ -0,0 +1,31 @@
+export const categoryColors = {
+  'Food': '#FF6B6B',
+  'Social Life': '#4ECDC4',
+  'Entertainment': '#45B7D1',
+  'Fuel': '#FFA726',
+  'Culture': '#66BB6A',
+  'Household': '#42A5F5',
+  'Apparel': '#AB47BC',
+  'Beauty': '#EC407A',
+  'Health': '#26A69A',
+  'Education': '#5C6BC0',
+  'Transportation': '#78909C',
+  'Vyomi': '#26C6DA',
+  'Vacation': '#FF7043',
+  'Subscriptions': '#9CCC65',
+  'Misc': '#8D6E63',
+  'Income': '#66BB6A'
+};
+
+export const masterCategories = [
+  { main: 'Food', sub: 'Food', combined: 'Food > Food' },
+  { main: 'Fuel', sub: 'Honda City', combined: 'Fuel > Honda City' },
+  { main: 'Fuel', sub: 'Aviator', combined: 'Fuel > Aviator' },
+  { main: 'Culture', sub: 'Culture', combined: 'Culture > Culture' },
+  { main: 'Household', sub: 'Grocery', combined: 'Household > Grocery' },
+  { main: 'Health', sub: 'Medical', combined: 'Health > Medical' },
+  { main: 'Income', sub: 'Income', combined: 'Income > Income' },
+  { main: 'Social Life', sub: 'Social Life', combined: 'Social Life > Social Life' },
+  { main: 'Entertainment', sub: 'Entertainment', combined: 'Entertainment > Entertainment' }
+  // Add more categories as needed
+];
